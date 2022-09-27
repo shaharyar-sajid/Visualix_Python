c2e6e139b7eb846cdd864ccdc92fc6146c8cc3ab
Shary <shaharyarsajid@gmail.com>
2022-09-27 18:08:52 +0500

    Replaced HTML forms with wtf_form Added bootstrap 4 thorugh bootstrap_flask Added Nav bar and Page header Added file validation to file upload Changed styling of webpage

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,5 +3,5 @@ from flask_wtf.file import FileField, FileRequired
 from wtforms import SubmitField
 
 class CsvForm(FlaskForm):
-    photo = FileField(validators=[FileRequired()])
+    csv = FileField('CSV File needed', validators=[FileRequired()], name='file')
     submit = SubmitField('Upload File')
--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,10 +1,10 @@
 import os
+from flask_app.forms import CsvForm
 from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_app import app
 from werkzeug.utils import secure_filename
 import pandas as pd
 from visualizations import util, flask_visual
-from flask_app.forms import CsvForm
 
 # @app.route('/')
 # @app.route('/index')
@@ -12,25 +12,53 @@ from flask_app.forms import CsvForm
 #     return render_template('index.html', title='Home')
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/index')
-def upload_files():
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    form = CsvForm()
     if request.method == 'POST':
-        form = CsvForm()
-        df = None
-        uploaded_file = request.files['file']
-        filename = secure_filename(uploaded_file.filename)
-        if filename != '':
-            file_ext = os.path.splitext(filename)[1]
-            if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-                abort(400)
-            uploaded_file.stream.seek(0) # seek to the beginning of file
-            #df = pd.read_csv(uploaded_file.stream)
-            df = util.load_stream_file(uploaded_file.stream)
-            util.split_title(df)
-            util.split_date(df)
-            util.make_day_categorical(df)
-            day_fig = flask_visual.visual_day_activity(df)
-        #return render_template('index.html', title='Home', tables=[df.to_html(classes='data')], titles=df.columns.values, day_fig=day_fig)
-        return render_template('index.html', title='Home', day_fig=day_fig)
 
-    return render_template('index.html', title='Home')
+        if form.validate_on_submit():
+            uploaded_file = form.csv.data
+            df = None
+            uploaded_file = request.files['file']
+            filename = secure_filename(uploaded_file.filename)
+            if filename != '':
+                file_ext = os.path.splitext(filename)[1]
+                if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+                    abort(400)
+                uploaded_file.stream.seek(0) # seek to the beginning of file
+                #df = pd.read_csv(uploaded_file.stream)
+                df = util.load_stream_file(uploaded_file.stream)
+                util.split_title(df)
+                util.split_date(df)
+                util.make_day_categorical(df)
+                day_fig = flask_visual.visual_day_activity(df)
+            #return render_template('index.html', title='Home', tables=[df.to_html(classes='data')], titles=df.columns.values, day_fig=day_fig)
+            return render_template('index.html', title='Home', day_fig=day_fig, form=form)
+        
+        # df = None
+        # uploaded_file = request.files['file']
+        # filename = secure_filename(uploaded_file.filename)
+        # if filename != '':
+        #     file_ext = os.path.splitext(filename)[1]
+        #     if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+        #         abort(400)
+        #     uploaded_file.stream.seek(0) # seek to the beginning of file
+        #     #df = pd.read_csv(uploaded_file.stream)
+        #     df = util.load_stream_file(uploaded_file.stream)
+        #     util.split_title(df)
+        #     util.split_date(df)
+        #     util.make_day_categorical(df)
+        #     day_fig = flask_visual.visual_day_activity(df)
+        # #return render_template('index.html', title='Home', tables=[df.to_html(classes='data')], titles=df.columns.values, day_fig=day_fig)
+        # return render_template('index.html', title='Home', day_fig=day_fig)
+
+    return render_template('index.html', title='Home', form=form)
+
+@app.route('/instruct')
+def instruct():
+    pass
+
+@app.route('/about')
+def about():
+    pass
