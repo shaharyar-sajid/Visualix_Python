50ddad08625bf5d3d1098147a7833a0fa854db43
Shary <shaharyarsajid@gmail.com>
2022-09-28 18:41:58 +0500

    Added new files

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config(object):
     MAX_CONTENT_LENGTH = 1024 * 1024
     UPLOAD_EXTENSIONS = ['.csv']
