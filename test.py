5d88817ffa9ab23f534fb7953d7dc56846d80dbd
Shary <shaharyarsajid@gmail.com>
2022-09-28 18:22:50 +0500

    Added requirements.txt file Made changes to test.py

--- test.py
+++ test.py
@@ -1,25 +1,25 @@
-from visualizations import util
-from visualizations.visualizations import visual_content_prod_country, visual_day_activity, visual_map_prod_country, visual_top_ten_genre, visual_top_ten_timeline, visual_top_ten_view, visual_viewing_timeline
-from icecream import ic
+# from visualizations import util
+# from visualizations.visualizations import visual_content_prod_country, visual_day_activity, visual_map_prod_country, visual_top_ten_genre, visual_top_ten_timeline, visual_top_ten_view, visual_viewing_timeline
+# from icecream import ic
 
-path = 'csv_files/NetflixViewingHistory.csv'
+# path = 'csv_files/NetflixViewingHistory.csv'
 
-df = util.load_csv_file(path)
-ic('Load success')
-util.split_title(df)
-util.split_date(df)
-ic(util.make_day_categorical(df))
-ic('Split success')
-#genres, movie_countries = util.get_imdb_genre_and_country(df)
-#countries = util.get_country_list(movie_countries=movie_countries)
-#ic(util.make_genre_column(df,genres))
-ic('got info')
-#ic(visual_content_prod_country(countries))
-ic(visual_day_activity(df))
-#visual_map_prod_country(countries)
-#visual_top_ten_genre(df)
-visual_top_ten_view(df)
-visual_viewing_timeline(df)
-visual_top_ten_timeline(df)
-ic('info viewed')
-#print(df.head())
+# df = util.load_csv_file(path)
+# ic('Load success')
+# util.split_title(df)
+# util.split_date(df)
+# ic(util.make_day_categorical(df))
+# ic('Split success')
+# #genres, movie_countries = util.get_imdb_genre_and_country(df)
+# #countries = util.get_country_list(movie_countries=movie_countries)
+# #ic(util.make_genre_column(df,genres))
+# ic('got info')
+# #ic(visual_content_prod_country(countries))
+# ic(visual_day_activity(df))
+# #visual_map_prod_country(countries)
+# #visual_top_ten_genre(df)
+# visual_top_ten_view(df)
+# visual_viewing_timeline(df)
+# visual_top_ten_timeline(df)
+# ic('info viewed')
+# #print(df.head())
