a6a24109c8b4ba0c4dc4560cd55fd0c2e66a4594
Shary <shaharyarsajid@gmail.com>
2022-09-28 16:41:17 +0500

    Created gitignore file Created about.html file Added three new images to static folder Added routes for about.html and instruct.html Developed the instruct.html file

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,15 +1,12 @@
+from dis import Instruction
 import os
+from turtle import title
 from flask_app.forms import CsvForm
 from flask import Flask, render_template, request, redirect, url_for, abort
 from flask_app import app
 from werkzeug.utils import secure_filename
 import pandas as pd
 from visualizations import util, flask_visual
-
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     return render_template('index.html', title='Home')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -27,7 +24,6 @@ def index():
                 if file_ext not in app.config['UPLOAD_EXTENSIONS']:
                     abort(400)
                 uploaded_file.stream.seek(0) # seek to the beginning of file
-                #df = pd.read_csv(uploaded_file.stream)
 
                 df = util.load_stream_file(uploaded_file.stream)
                 util.split_title(df)
@@ -41,30 +37,13 @@ def index():
                 graphs.append(flask_visual.visual_top_ten_timeline_flask(df))
             #return render_template('index.html', title='Home', tables=[df.to_html(classes='data')], titles=df.columns.values, day_fig=day_fig)
             return render_template('index.html', title='Home', graphs=graphs, form=form)
-        
-        # df = None
-        # uploaded_file = request.files['file']
-        # filename = secure_filename(uploaded_file.filename)
-        # if filename != '':
-        #     file_ext = os.path.splitext(filename)[1]
-        #     if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-        #         abort(400)
-        #     uploaded_file.stream.seek(0) # seek to the beginning of file
-        #     #df = pd.read_csv(uploaded_file.stream)
-        #     df = util.load_stream_file(uploaded_file.stream)
-        #     util.split_title(df)
-        #     util.split_date(df)
-        #     util.make_day_categorical(df)
-        #     day_fig = flask_visual.visual_day_activity(df)
-        # #return render_template('index.html', title='Home', tables=[df.to_html(classes='data')], titles=df.columns.values, day_fig=day_fig)
-        # return render_template('index.html', title='Home', day_fig=day_fig)
 
     return render_template('index.html', title='Home', form=form)
 
 @app.route('/instruct')
 def instruct():
-    pass
+    return render_template('instruct.html', title='Instructions')
 
 @app.route('/about')
 def about():
-    pass
+    return render_template('about.html', title='About')
