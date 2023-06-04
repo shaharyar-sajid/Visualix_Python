dbab15f092cb444ea4dba8c0cb2e072f0e33bc37
Shary <shaharyarsajid@gmail.com>
2023-06-04 21:26:06 +0500

    added docstring to class

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -3,5 +3,14 @@ from flask_wtf.file import FileField, FileRequired
 from wtforms import SubmitField
 
 class CsvForm(FlaskForm):
+    '''
+    Form for uploading csv file
+
+    Parameters
+    ----------
+    FlaskForm : object
+        FlaskForm object
+    '''
+
     csv = FileField('CSV File needed', validators=[FileRequired()], name='file')
     submit = SubmitField('Upload File')
